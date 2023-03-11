411a2b5f1d7413112b03662a44cee37634238db7
john <johnsantosh2@gmail.com>
2023-03-11 21:18:02 +0530

    UI updates and calculator arithmetic logics updated

--- script/main.js
+++ script/main.js
@@ -24,56 +24,76 @@ let multiply = document.querySelector('#mul')
 let equalTo = document.querySelector('#equal')
 let operator = document.querySelectorAll(".operator");
 let result = document.querySelector(".result")
-
+let keys = document.querySelectorAll(".key");
 function operate(a,b,operator){
     return operator(a,b)
 }
 // console.log(operate(5, 6, add));
 
-let num1;
-let num2;
-let operator1;
+let number1 = "" ;
+let number2 = "";
+let operator1 ="";
+let displayResult = "";
 
 // for numbers
-let eve = document.querySelectorAll('.key')
+let eve = document.querySelectorAll('.input')
 let display = document.querySelector('.display')
 
 eve.forEach(each=>{
     each.addEventListener('click',e=>{
-        // if(!a) { b = e.target.value}
-        // console.log("john")
         a = e.target.value;
-        // console.log(a,b)
-        // if(a === "%" || a === "/" || a ==="+" || a == "-" || a ==="*"){
-        //     console.log(e.target.getAttribute("id"))
-        // }
-        // console.log(a)
-        // if(operator1){num2=a;return;}
-        if(Number.isInteger(Number(a))||a===".")
-        {  
-            if(operator1){
-                num2=a
-            }else num1=a
-            // console.log(num1)
-        }
-        display.textContent += ` ${e.target.value}`;
-        console.log(num1 , operator1 , num2)
+
+        if(Number.isInteger(Number(a))||a===".")number2 += a
+
+        display.textContent += `${e.target.value}`;
     })
 })
+window.addEventListener('keydown',e=>{
+    keys.forEach((each) => {
+        // let reg = /\/?\*?-?\+?%?/;
+        let eKey = e.key;
+        if(each.textContent == eKey) {
 
-clear.addEventListener('click',()=>display.textContent="0")
+            if(Number.isInteger(Number(eKey))||eKey==="."){
+                display.textContent += `${eKey}`;
+                number2 += eKey;
+            }
+            else {
+                operator1 = each.getAttribute("id");
+                number1 = number2;
+                number2 = "";
+                display.textContent += `${eKey}`;
+            }
+        }
+        
+    });
+})
+clear.addEventListener('click',()=>display.textContent="")
 
 // console.log(operator)
 operator.forEach(each=>{
     each.addEventListener("click", (e) => {
-        console.log(e.target.getAttribute("id"))
-        // display.textContent+=e.target.value
+        number1 = number2;
+        number2= "";
+        display.textContent += e.target.textContent
         operator1 = e.target.getAttribute("id");
     });
 })
 
 // 
 equalTo.addEventListener('click',()=>{
-    console.log(num1,num2,operator1)
-    result.textContent = operate(10,20,["sub"])
+    console.log(number1,number2,operator1)
+    displayResult = operate(Number(number1),Number(number2),window[operator1]); 
+    result.textContent = displayResult;
+        display.textContent = "";
+        number1 = "";
+        number2 = "";
+})
+
+ac.addEventListener('click',()=>{
+    number1= "";
+    number2="";
+    operator1 = "";
+    result.textContent=0;
+    display.textContent="";
 })
